02898350430b928bb6c1dcbdb00f05f656c1271e
Lightwell-bg <fsignbg@gmail.com>
2021-01-17 18:07:38 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,278 @@
+#include <Arduino.h>
+
+#include "config.h"
+#include "utility.h"
+#include "noiseEffects.h"
+#include "effects.h"
+#include "effectTicker.h"
+#include "runningText.h"
+#include "time.h"
+#include "parsing.h"
+#include "EepromManager.h"
+#include "TelnetManager.h"
+
+#ifdef OTA
+OtaManager otaManager(&showWarning);
+OtaPhase OtaManager::OtaFlag = OtaPhase::None;
+#endif
+#include "button.h"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  ESP.wdtEnable(WDTO_8S);
+  // ПИНЫ
+  #ifdef MOSFET_PIN                                         // инициализация пина, управляющего MOSFET транзистором в состояние "выключен"
+  pinMode(MOSFET_PIN, OUTPUT);
+  #ifdef MOSFET_LEVEL
+  digitalWrite(MOSFET_PIN, !MOSFET_LEVEL);
+  #endif
+  #endif
+
+  #ifdef ALARM_PIN                                          // инициализация пина, управляющего будильником в состояние "выключен"
+  pinMode(ALARM_PIN, OUTPUT);
+  #ifdef ALARM_LEVEL
+  digitalWrite(ALARM_PIN, !ALARM_LEVEL);
+  #endif
+  #endif
+  // TELNET
+  #if defined(GENERAL_DEBUG) && GENERAL_DEBUG_TELNET
+  telnetServer.begin();
+  for (uint8_t i = 0; i < 100; i++)                         // пауза 10 секунд в отладочном режиме, чтобы успеть подключиться по протоколу telnet до вывода первых сообщений
+  {
+    handleTelnetClient();
+    delay(100);
+    ESP.wdtFeed();
+  }
+  #endif
+  // КНОПКА
+  #if defined(ESP_USE_BUTTON)
+  touch.setStepTimeout(BUTTON_STEP_TIMEOUT);
+  touch.setClickTimeout(BUTTON_CLICK_TIMEOUT);
+    #if ESP_RESET_ON_START
+    delay(1000);                                            // ожидание инициализации модуля кнопки ttp223 (по спецификации 250мс)
+    if (digitalRead(BTN_PIN))
+    {
+      wifiManager.resetSettings();                          // сброс сохранённых SSID и пароля при старте с зажатой кнопкой, если разрешено
+      LOG.println(F("Настройки WiFiManager сброшены"));
+    }
+    buttonEnabled = true;                                   // при сбросе параметров WiFi сразу после старта с зажатой кнопкой, также разблокируется кнопка, если была заблокирована раньше
+    EepromManager::SaveButtonEnabled(&buttonEnabled);
+    ESP.wdtFeed();
+    #endif
+  #endif
+
+
+  // ЛЕНТА/МАТРИЦА
+  FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)/*.setCorrection(TypicalLEDStrip)*/;
+  FastLED.setBrightness(BRIGHTNESS);
+  if (CURRENT_LIMIT > 0)
+  {
+    FastLED.setMaxPowerInVoltsAndMilliamps(5, CURRENT_LIMIT);
+  }
+  FastLED.clear();
+  FastLED.show();
+
+
+  // EEPROM
+  EepromManager::InitEepromSettings(                        // инициализация EEPROM; запись начального состояния настроек, если их там ещё нет; инициализация настроек лампы значениями из EEPROM
+    modes, alarms, &espMode, &ONflag, &dawnMode, &currentMode, &buttonEnabled,
+    &(FavoritesManager::ReadFavoritesFromEeprom),
+    &(FavoritesManager::SaveFavoritesToEeprom));
+  LOG.printf_P(PSTR("Рабочий режим лампы: ESP_MODE = %d\n"), espMode);
+
+
+  // WI-FI
+  wifiManager.setDebugOutput(WIFIMAN_DEBUG);                // вывод отладочных сообщений
+  // wifiManager.setMinimumSignalQuality();                 // установка минимально приемлемого уровня сигнала WiFi сетей (8% по умолчанию)
+  CaptivePortalManager *captivePortalManager = new CaptivePortalManager(&wifiManager);
+  if (espMode == 0U)                                        // режим WiFi точки доступа
+  {
+    // wifiManager.setConfigPortalBlocking(false);
+
+    if (sizeof(AP_STATIC_IP))
+    {
+      LOG.println(F("Используется статический IP адрес WiFi точки доступа"));
+      wifiManager.setAPStaticIPConfig(                      // wifiManager.startConfigPortal использовать нельзя, т.к. он блокирует вычислительный процесс внутри себя, а затем перезагружает ESP, т.е. предназначен только для ввода SSID и пароля
+        IPAddress(AP_STATIC_IP[0], AP_STATIC_IP[1], AP_STATIC_IP[2], AP_STATIC_IP[3]),      // IP адрес WiFi точки доступа
+        IPAddress(AP_STATIC_IP[0], AP_STATIC_IP[1], AP_STATIC_IP[2], 1),                    // первый доступный IP адрес сети
+        IPAddress(255, 255, 255, 0));                                                       // маска подсети
+    }
+
+    WiFi.softAP(AP_NAME, AP_PASS);
+
+    LOG.println(F("Старт в режиме WiFi точки доступа"));
+    LOG.print(F("IP адрес: "));
+    LOG.println(WiFi.softAPIP());
+
+    wifiServer.begin();
+  }
+  else                                                      // режим WiFi клиента (подключаемся к роутеру, если есть сохранённые SSID и пароль, иначе создаём WiFi точку доступа и запрашиваем их)
+  {
+    LOG.println(F("Старт в режиме WiFi клиента (подключение к роутеру)"));
+
+    if (WiFi.SSID().length())
+    {
+      LOG.printf_P(PSTR("Подключение к WiFi сети: %s\n"), WiFi.SSID().c_str());
+
+      if (sizeof(STA_STATIC_IP))                            // ВНИМАНИЕ: настраивать статический ip WiFi клиента можно только при уже сохранённых имени и пароле WiFi сети (иначе проявляется несовместимость библиотек WiFiManager и WiFi)
+      {
+        LOG.print(F("Сконфигурирован статический IP адрес: "));
+        LOG.printf_P(PSTR("%u.%u.%u.%u\n"), STA_STATIC_IP[0], STA_STATIC_IP[1], STA_STATIC_IP[2], STA_STATIC_IP[3]);
+        wifiManager.setSTAStaticIPConfig(
+          IPAddress(STA_STATIC_IP[0], STA_STATIC_IP[1], STA_STATIC_IP[2], STA_STATIC_IP[3]),// статический IP адрес ESP в режиме WiFi клиента
+          IPAddress(STA_STATIC_IP[0], STA_STATIC_IP[1], STA_STATIC_IP[2], 1),               // первый доступный IP адрес сети (справедливо для 99,99% случаев; для сетей меньше чем на 255 адресов нужно вынести в константы)
+          IPAddress(255, 255, 255, 0));                                                     // маска подсети (справедливо для 99,99% случаев; для сетей меньше чем на 255 адресов нужно вынести в константы)
+      }
+    }
+    else
+    {
+      LOG.println(F("WiFi сеть не определена, запуск WiFi точки доступа для настройки параметров подключения к WiFi сети..."));
+      CaptivePortalManager::captivePortalCalled = true;
+      wifiManager.setBreakAfterConfig(true);                // перезагрузка после ввода и сохранения имени и пароля WiFi сети
+      showWarning(CRGB::Yellow, 1000U, 500U);               // мигание жёлтым цветом 0,5 секунды (1 раз) - нужно ввести параметры WiFi сети для подключения
+    }
+
+    wifiManager.setConnectTimeout(ESP_CONN_TIMEOUT);        // установка времени ожидания подключения к WiFi сети, затем старт WiFi точки доступа
+    wifiManager.setConfigPortalTimeout(ESP_CONF_TIMEOUT);   // установка времени работы WiFi точки доступа, затем перезагрузка; отключить watchdog?
+    wifiManager.autoConnect(AP_NAME, AP_PASS);              // пытаемся подключиться к сохранённой ранее WiFi сети; в случае ошибки, будет развёрнута WiFi точка доступа с указанными AP_NAME и паролем на время ESP_CONN_TIMEOUT секунд; http://AP_STATIC_IP:ESP_HTTP_PORT (обычно http://192.168.0.1:80) - страница для ввода SSID и пароля от WiFi сети роутера
+
+    delete captivePortalManager;
+    captivePortalManager = NULL;
+
+    if (WiFi.status() != WL_CONNECTED)                      // подключение к WiFi не установлено
+    {
+      if (CaptivePortalManager::captivePortalCalled)        // была показана страница настройки WiFi ...
+      {
+        if (millis() < (ESP_CONN_TIMEOUT + ESP_CONF_TIMEOUT) * 1000U) // пользователь ввёл некорректное имя WiFi сети и/или пароль или запрошенная WiFi сеть недоступна
+        {
+          LOG.println(F("Не удалось подключиться к WiFi сети\nУбедитесь в корректности имени WiFi сети и пароля\nРестарт для запроса нового имени WiFi сети и пароля...\n"));
+          wifiManager.resetSettings();
+        }
+        else                                                // пользователь не вводил имя WiFi сети и пароль
+        {
+          LOG.println(F("Время ожидания ввода SSID и пароля от WiFi сети или подключения к WiFi сети превышено\nЛампа будет перезагружена в режиме WiFi точки доступа!\n"));
+
+          espMode = (espMode == 0U) ? 1U : 0U;
+          EepromManager::SaveEspMode(&espMode);
+
+          LOG.printf_P(PSTR("Рабочий режим лампы изменён и сохранён в энергонезависимую память\nНовый рабочий режим: ESP_MODE = %d, %s\nРестарт...\n"),
+            espMode, espMode == 0U ? F("WiFi точка доступа") : F("WiFi клиент (подключение к роутеру)"));
+        }
+      }
+      else                                                  // страница настройки WiFi не была показана, не удалось подключиться к ранее сохранённой WiFi сети (перенос в новую WiFi сеть)
+      {
+        LOG.println(F("Не удалось подключиться к WiFi сети\nВозможно, заданная WiFi сеть больше не доступна\nРестарт для запроса нового имени WiFi сети и пароля...\n"));
+        wifiManager.resetSettings();
+      }
+
+      showWarning(CRGB::Red, 1000U, 500U);                  // мигание красным цветом 0,5 секунды (1 раз) - ожидание ввода SSID'а и пароля WiFi сети прекращено, перезагрузка
+      ESP.restart();
+    }
+
+    if (CaptivePortalManager::captivePortalCalled &&        // первое подключение к WiFi сети после настройки параметров WiFi на странице настройки - нужна перезагрузка для применения статического IP
+        sizeof(STA_STATIC_IP) &&
+        WiFi.localIP() != IPAddress(STA_STATIC_IP[0], STA_STATIC_IP[1], STA_STATIC_IP[2], STA_STATIC_IP[3]))
+    {
+      LOG.println(F("Рестарт для применения заданного статического IP адреса..."));
+      delay(100);
+      ESP.restart();
+    }
+
+    LOG.print(F("IP адрес: "));
+    LOG.println(WiFi.localIP());
+  }
+  ESP.wdtFeed();
+
+  LOG.printf_P(PSTR("Порт UDP сервера: %u\n"), localPort);
+  Udp.begin(localPort);
+
+
+  // NTP
+  #ifdef USE_NTP
+  timeClient.begin();
+  ESP.wdtFeed();
+  #endif
+
+
+  // MQTT
+  #if (USE_MQTT)
+  if (espMode == 1U)
+  {
+    mqttClient = new AsyncMqttClient();
+    MqttManager::setupMqtt(mqttClient, inputBuffer, &sendCurrent);    // создание экземпляров объектов для работы с MQTT, их инициализация и подключение к MQTT брокеру
+  }
+  ESP.wdtFeed();
+  #endif
+
+
+  // ОСТАЛЬНОЕ
+  memset(matrixValue, 0, sizeof(matrixValue));
+  randomSeed(micros());
+  changePower();
+  loadingFlag = true;
+}
+
+
+void loop() {
+  parseUDP();
+  effectsTick();
+
+  EepromManager::HandleEepromTick(&settChanged, &eepromTimeout, &ONflag, 
+    &currentMode, modes, &(FavoritesManager::SaveFavoritesToEeprom));
+
+  #ifdef USE_NTP
+  timeTick();
+  #endif
+
+  #ifdef ESP_USE_BUTTON
+  if (buttonEnabled)  {
+    buttonTick();
+  }
+  #endif
+
+  #ifdef OTA
+  otaManager.HandleOtaUpdate();                             // ожидание и обработка команды на обновление прошивки по воздуху
+  #endif
+
+  TimerManager::HandleTimer(&ONflag, &settChanged,          // обработка событий таймера отключения лампы
+    &eepromTimeout, &changePower);
+
+  if (FavoritesManager::HandleFavorites(                    // обработка режима избранных эффектов
+      &ONflag,
+      &currentMode,
+      &loadingFlag
+      #ifdef USE_NTP
+      , &dawnFlag
+      #endif
+      ))
+  {
+    FastLED.setBrightness(modes[currentMode].Brightness);
+    FastLED.clear();
+    delay(1);
+  }
+
+  #if USE_MQTT
+  if (espMode == 1U && mqttClient && WiFi.isConnected() && !mqttClient->connected())
+  {
+    MqttManager::mqttConnect();                             // библиотека не умеет восстанавливать соединение в случае потери подключения к MQTT брокеру, нужно управлять этим явно
+    MqttManager::needToPublish = true;
+  }
+
+  if (MqttManager::needToPublish)
+  {
+    if (strlen(inputBuffer) > 0)                            // проверка входящего MQTT сообщения; если оно не пустое - выполнение команды из него и формирование MQTT ответа
+    {
+      processInputBuffer(inputBuffer, MqttManager::mqttBuffer, true);
+    }
+    
+    MqttManager::publishState();
+  }
+  #endif
+
+  #if defined(GENERAL_DEBUG) && GENERAL_DEBUG_TELNET
+  handleTelnetClient();
+  #endif
+
+  ESP.wdtFeed();                                            // пнуть собаку
+}
